213a65cbcbc8b06122f8722983d4b24f0547ab66
Kent Fredric <kentfredric@gmail.com>
2015-06-22 09:22:09 +1200

    Build results of a0a126f (on master)

---
+++ lib/Dist/Zilla/Plugin/MetaYAML/Minimal.pm
@@ -0,0 +1,45 @@
+use 5.006;  # our
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::MetaYAML::Minimal;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT: Kent Failed To Provide An Abstract
+
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
+
+use Moose;
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::MetaYAML::Minimal - Kent Failed To Provide An Abstract
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
