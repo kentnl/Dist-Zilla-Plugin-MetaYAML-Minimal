1ec95769dda352ca580e8da3935217c65f80c969
Kent Fredric <kentfredric@gmail.com>
2015-06-22 09:52:24 +1200

    First version of fully functional code without tests

--- lib/Dist/Zilla/Plugin/MetaYAML/Minimal.pm
+++ lib/Dist/Zilla/Plugin/MetaYAML/Minimal.pm
@@ -1,4 +1,4 @@
-use 5.006;  # our
+use 5.006;    # our
 use strict;
 use warnings;
 
@@ -6,13 +6,104 @@ package Dist::Zilla::Plugin::MetaYAML::Minimal;
 
 our $VERSION = '0.001000';
 
-# ABSTRACT: Kent Failed To Provide An Abstract
+# ABSTRACT: Generate a reductionist YAML META file for compatibility only
 
 # AUTHORITY
 
-use Moose;
+use Moose qw( has with around );
+use Try::Tiny qw( try catch );
+use Dist::Zilla::Util::ConfigDumper qw( config_dumper );
+
+with 'Dist::Zilla::Role::FileGatherer';
+
+has filename => (
+  is      => 'ro',
+  isa     => 'Str',
+  default => 'META.yml',
+);
+
+has version => (
+  is      => 'ro',
+  isa     => 'Num',
+  default => '1.4',
+);
+
+around dump_config => config_dumper( __PACKAGE__, qw( filename version ) );
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
+=for Pod::Coverage gather_files
+
+=cut
+
+sub gather_files {
+  my ($self,) = @_;
+
+  require Dist::Zilla::File::FromCode;
+  require YAML::Tiny;
+  require CPAN::Meta::Converter;
+  CPAN::Meta::Converter->VERSION(2.101550);    # improved downconversion
+  require CPAN::Meta::Validator;
+  CPAN::Meta::Validator->VERSION(2.101550);    # improved downconversion
+
+  my $zilla = $self->zilla;
+
+  my $file  = Dist::Zilla::File::FromCode->new({
+    name => $self->filename,
+    code_return_type => 'text',
+    code => sub {
+      my $distmeta = $zilla->distmeta;
+
+      my $validator = CPAN::Meta::Validator->new($distmeta);
+
+      if ( not $validator->is_valid ) {
+        my $msg = "Invalid META structure.  Errors found:\n";
+        $msg .= join "\n", $validator->errors;
+        $self->log_fatal($msg);
+      }
+
+      my $converter = CPAN::Meta::Converter->new($distmeta);
+      my $output    = $converter->convert( version => $self->version );
+
+      for my $key ( keys %{$output} ) {
+        delete $output->{$key} if $key =~ /\Ax_/sx;
+      }
+
+      my $yaml = try {
+        YAML::Tiny->new($output)->write_string; # text!
+      }
+      catch {
+        $self->log_fatal('Could not create YAML string: ' . YAML::Tiny->errstr);
+      };
+      return $yaml;
+    },
+  });
+
+  $self->add_file($file);
+  return;
+}
+
 1;
+
+=head1 DESCRIPTION
+
+Generally, if you're creating both C<META.json> and C<META.yml>, then you're doing so purely for compatibility reasons.
+
+In such circumstances, using the same meta-data for both leads to a lot of cruft in C<META.yml>
+
+This L<C<Dist::Zilla>|Dist::Zilla> extension is for such circumstances.
+
+However, if you are I<only> shipping C<META.yml> and B<NOT> C<META.json>, then using this extension
+would be harmful and cause loss of information.
+
+Presently, this extension is a I<PROTOTYPE>, and just culls fields leading with C<x_> passed by C<Dist::Zilla>.
+
+The final behavior may require enhancements to C<CPAN::Meta::Converter> and might be possibly superseded
+by patches to C<MetaYAML> itself.
+
+=head1 SYNOPSIS
+
+  [MetaYAML::Minimal]
+  filename = META.yml ; default
+  version  = 1.4      ; default
